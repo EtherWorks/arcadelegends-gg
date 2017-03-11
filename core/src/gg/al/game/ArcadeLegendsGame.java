ff20aab4ac7bcdc6f30816876a9bacd8b4651e96
AsterAether <tommi99.tn@gmail.com>
2017-03-11 19:39:27 +0100

    Added real launcher class.

---
+++ core/src/gg/al/game/ArcadeLegendsGame.java
@@ -0,0 +1,13 @@
+package gg.al.game;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by Dr. Gavrel on 11.03.2017.
+ */
+public class ArcadeLegendsGame extends Game {
+    @Override
+    public void create() {
+
+    }
+}
--- desktop/src/gg/al/desktop/launcher/DesktopLauncher.java
+++ desktop/src/gg/al/desktop/launcher/DesktopLauncher.java
@@ -2,11 +2,11 @@ package gg.al.desktop.launcher;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import gg.al.prototype.ArcadeLegends;
+import gg.al.game.ArcadeLegendsGame;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        LwjglApplication application = new LwjglApplication(new ArcadeLegends(), config);
+        LwjglApplication application = new LwjglApplication(new ArcadeLegendsGame(), config);
     }
 }
