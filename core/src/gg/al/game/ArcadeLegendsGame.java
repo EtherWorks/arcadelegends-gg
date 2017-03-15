e44e4f11a8929840b929b30ec7ff3462582d96c0
AsterAether <tommi99.tn@gmail.com>
2017-03-15 11:27:06 +0100

    Added cfg4j

--- core/src/gg/al/game/ArcadeLegendsGame.java
+++ core/src/gg/al/game/ArcadeLegendsGame.java
@@ -1,12 +1,10 @@
 package gg.al.game;
 
 import com.badlogic.gdx.Game;
-import lombok.extern.slf4j.Slf4j;
 
 /**
- * Created by Dr. Gavrel on 11.03.2017.
+ * Created by Thomas Neumann on 11.03.2017.
  */
-@Slf4j
 public class ArcadeLegendsGame extends Game {
 
     @Override
--- desktop/src/gg/al/desktop/launcher/DesktopLauncher.java
+++ desktop/src/gg/al/desktop/launcher/DesktopLauncher.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import gg.al.game.ArcadeLegendsGame;
 
+/**
+ * Created by Thomas Neumann on 11.03.2017.
+ */
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
