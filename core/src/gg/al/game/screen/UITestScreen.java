4bfbd1053a6f61699f3e0580bd6047ea87b47dba
Patrick <winpab13@htl-kaindorf.ac.at>
2017-03-16 13:09:36 +0100

    Added ArcadeScreen and UITestScreen to prework

---
+++ core/src/gg/al/game/screen/UITestScreen.java
@@ -0,0 +1,48 @@
+package gg.al.game.screen;
+
+import gg.al.game.ArcadeLegendsGame;
+
+/**
+ * Created by Patrick Windegger on 16.03.2017.
+ */
+public class UITestScreen extends ArcadeScreen {
+
+    public UITestScreen(ArcadeLegendsGame game) {
+        super(game);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
