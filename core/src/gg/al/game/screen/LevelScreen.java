9ee5ebfff3ffd702082909517f00181b52d5b190
AsterAether <tommi99.tn@gmail.com>
2017-03-20 23:15:00 +0100

    Added rendering of TileMap in 3D space

--- core/src/gg/al/game/screen/LevelScreen.java
+++ core/src/gg/al/game/screen/LevelScreen.java
@@ -1,14 +1,13 @@
 package gg.al.game.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
 import com.badlogic.gdx.graphics.g3d.decals.Decal;
 import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -28,9 +27,8 @@ import java.util.List;
 public class LevelScreen implements IAssetScreen {
 
     private final AssetDescriptor<TiledMap> mapDesc;
+    private final float rot;
     private TiledMap map;
-    private OrthographicCamera mapCam;
-    private Viewport viewportMap;
 
     private OrthogonalTiledMapRenderer mapRenderer;
 
@@ -41,8 +39,14 @@ public class LevelScreen implements IAssetScreen {
     private Viewport viewport;
 
     private Decal mapDecal;
+    private TextureRegion mapTemp;
 
     public LevelScreen(AssetDescriptor<TiledMap> mapDesc) {
+        this(mapDesc, 15);
+    }
+
+    public LevelScreen(AssetDescriptor mapDesc, float rot) {
+        this.rot = rot;
         this.mapDesc = mapDesc;
     }
 
@@ -59,15 +63,17 @@ public class LevelScreen implements IAssetScreen {
     @Override
     public void show() {
         camera = new PerspectiveCamera();
+
+        //camera.rotate(Vector3.X, rot);
         camera.position.set(0, 0, 10);
+        camera.fieldOfView = 15;
         viewport = new FitViewport(1920, 1080, camera);
-        viewport.update(viewport.getScreenWidth(), viewport.getScreenHeight(), true);
+        viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch = new DecalBatch(new CameraGroupStrategy(camera));
         map = AL.asset.get(mapDesc);
-        mapCam = new OrthographicCamera();
-        viewportMap = new FitViewport(map.getProperties().get("width", Integer.class) * map.getProperties().get("tilewidth", Integer.class),
+        OrthographicCamera mapCam = new OrthographicCamera();
+        Viewport viewportMap = new FitViewport(map.getProperties().get("width", Integer.class) * map.getProperties().get("tilewidth", Integer.class),
                 map.getProperties().get("height", Integer.class) * map.getProperties().get("tileheight", Integer.class), mapCam);
-
         viewportMap.update(viewportMap.getScreenWidth(), viewportMap.getScreenHeight(), true);
         mapRenderer = new OrthogonalTiledMapRenderer(map);
         mapRenderer.setView(mapCam);
@@ -76,27 +82,36 @@ public class LevelScreen implements IAssetScreen {
                 map.getProperties().get("height", Integer.class) * map.getProperties().get("tileheight", Integer.class),
                 false);
         mapDecal = Decal.newDecal(map.getProperties().get("width", Integer.class), map.getProperties().get("height", Integer.class), new TextureRegion());
+        mapDecal.setPosition(0, 0, 0);
+
+        Gdx.input.setInputProcessor(new CameraInputController(camera));
     }
 
     @Override
     public void render(float delta) {
-        AL.gl.glClearColor(0, 0, 0, 1);
-        AL.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        AL.graphics.getGL20().glClearColor(0, 0, 0, 1);
+        AL.graphics.getGL20().glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
         mapBuffer.begin();
+        AL.graphics.getGL20().glClearColor(0, 0, 0, 1);
+        AL.graphics.getGL20().glClear(GL20.GL_COLOR_BUFFER_BIT);
         mapRenderer.render();
         mapBuffer.end();
+        mapBuffer.getColorBufferTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
 
-        TextureRegion region = new TextureRegion(mapBuffer.getColorBufferTexture());
-        region.flip(false, true);
+        if (mapTemp == null) {
+            mapTemp = new TextureRegion(mapBuffer.getColorBufferTexture());
+            mapTemp.flip(false, true);
+        } else mapTemp.setTexture(mapBuffer.getColorBufferTexture());
 
-        mapDecal.setTextureRegion(region);
+        mapDecal.setTextureRegion(mapTemp);
         batch.add(mapDecal);
         batch.flush();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
